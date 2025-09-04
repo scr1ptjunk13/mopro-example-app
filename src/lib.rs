45197174e2a44ac0e2f7956929976c54d4c1dd5b
scr1ptjunk13 <itsameshreyansh@protonmail.com>
2025-09-05 01:01:28 +0530

    fix: removed pre-written multiplier2 with custom addn circ

--- src/lib.rs
+++ src/lib.rs
@@ -32,20 +32,20 @@ mod noir_tests {
 
     #[test]
     #[serial]
-    fn test_noir_multiplier2() {
-        let srs_path = "./test-vectors/noir/noir_multiplier2.srs".to_string();
-        let circuit_path = "./test-vectors/noir/noir_multiplier2.json".to_string();
-        let circuit_inputs = vec!["3".to_string(), "5".to_string()];
+    fn test_addition_circuit() {
+        let circuit_path = "./test-vectors/noir/addition_circuit.json".to_string();
+        let circuit_inputs = vec!["10".to_string(), "5".to_string()];
+        
         let vk = get_noir_verification_key(
             circuit_path.clone(),
-            Some(srs_path.clone()),
+            None,   // no srs needed for small circuits
             true,   // on_chain (uses Keccak for Solidity compatibility)
             false,  // low_memory_mode
         ).unwrap();
 
         let proof = generate_noir_proof(
             circuit_path.clone(),
-            Some(srs_path.clone()),
+            None,   // no srs needed for small circuits
             circuit_inputs.clone(),
             true,   // on_chain (uses Keccak for Solidity compatibility)
             vk.clone(),
@@ -61,46 +61,6 @@ mod noir_tests {
         ).unwrap();
         assert!(valid);
     }
-
-    #[test]
-    #[serial]
-    fn test_noir_multiplier2_with_existing_vk() {
-        let srs_path = "./test-vectors/noir/noir_multiplier2.srs".to_string();
-        let circuit_path = "./test-vectors/noir/noir_multiplier2.json".to_string();
-        let vk_path = "./test-vectors/noir/noir_multiplier2.vk".to_string();
-
-        // read vk from file as Vec<u8>
-        let vk = std::fs::read(vk_path).unwrap();
-
-        let circuit_inputs = vec!["3".to_string(), "5".to_string()];
-
-        let proof = generate_noir_proof(
-            circuit_path.clone(),
-            Some(srs_path),
-            circuit_inputs,
-            true,   // on_chain (uses Keccak for Solidity compatibility)
-            vk.clone(),
-            false,  // low_memory_mode
-        ).unwrap();
-
-        let valid = verify_noir_proof(
-            circuit_path,
-            proof,
-            true,   // on_chain (uses Keccak for Solidity compatibility)
-            vk,
-            false,  // low_memory_mode
-        ).unwrap();
-        assert!(valid);
-    }
 }
 
 
-#[cfg(test)]
-mod uniffi_tests {
-    use super::*;
-
-    #[test]
-    fn test_mopro_uniffi_hello_world() {
-        assert_eq!(mopro_uniffi_hello_world(), "Hello, World!");
-    }
-}
